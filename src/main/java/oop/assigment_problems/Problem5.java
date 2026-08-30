2af9ec307969e7737cb33669b42ff786216ab7bb
Ayush Mohan <ayushmohan86@gmail.com>
2026-08-30 18:09:43 +0530

    Add Week 3 assignment problems

---
+++ src/main/java/oop/assigment_problems/Problem5.java
@@ -0,0 +1,36 @@
+class Employee {
+
+    String empName;
+    double salary;
+
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+}
+
+public class Problem5 {
+
+    public static void main(String[] args) {
+
+        Employee employee1 =
+                new Employee("Rahul", 50000);
+
+        Employee employee2 =
+                new Employee("Priya", 60000);
+
+        Employee employee3 =
+                new Employee("Aman", 55000);
+
+        Employee.printCompanyInfo();
+    }
+}
