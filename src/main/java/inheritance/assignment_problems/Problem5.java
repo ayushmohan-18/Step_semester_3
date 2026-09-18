7197384ab548e984bcda069c08320c9adb7f4304
Ayush Mohan <ayushmohan86@gmail.com>
2026-09-19 00:11:58 +0530

    Add inheritance assignment solutions

---
+++ src/main/java/inheritance/assignment_problems/Problem5.java
@@ -0,0 +1,180 @@
+package main.java.inheritance.assignment_problems;
+
+class LibraryMember {
+
+    protected String memberId;
+    protected int borrowLimit;
+    protected int booksBorrowed;
+
+    private static int membersEnrolled = 100;
+
+    public final String memberNumber;
+
+    private String[] genres = new String[100];
+    private int genreCount = 0;
+
+
+    public LibraryMember(int borrowLimit) {
+
+        if (borrowLimit <= 0) {
+            throw new IllegalArgumentException(
+                "Borrow limit must be positive"
+            );
+        }
+
+        membersEnrolled++;
+
+        memberNumber = "LIB-" + membersEnrolled;
+
+        this.borrowLimit = borrowLimit;
+    }
+
+
+    public void borrowBook() {
+
+        if (booksBorrowed < borrowLimit) {
+            booksBorrowed++;
+        }
+    }
+
+
+    public void borrowBook(String genre) {
+
+        if (genreCount < genres.length) {
+            genres[genreCount] = genre;
+            genreCount++;
+        }
+
+        borrowBook();
+    }
+
+
+    public int getBooksBorrowed() {
+        return booksBorrowed;
+    }
+
+
+    public static boolean isValidRenewalCode(
+        String code
+    ) {
+
+        if (code == null || code.length() != 4) {
+            return false;
+        }
+
+        if (code.charAt(0) != 'R') {
+            return false;
+        }
+
+        if (!Character.isDigit(code.charAt(1))) {
+            return false;
+        }
+
+        if (!Character.isDigit(code.charAt(2))) {
+            return false;
+        }
+
+        if (!Character.isUpperCase(code.charAt(3))) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public static int getMembersEnrolled() {
+        return membersEnrolled - 100;
+    }
+}
+
+
+class FacultyMember extends LibraryMember {
+
+    private String department;
+
+    public FacultyMember(
+        int borrowLimit,
+        String department
+    ) {
+        super(borrowLimit);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}
+
+
+public class Problem5 {
+
+    public static String processNightlyAudit(
+        LibraryMember[] members
+    ) {
+
+        int processed = 0;
+        int nullSkipped = 0;
+        int faculty = 0;
+        int regular = 0;
+
+        for (LibraryMember member : members) {
+
+            if (member == null) {
+                nullSkipped++;
+                continue;
+            }
+
+            processed++;
+
+            if (member instanceof FacultyMember) {
+                faculty++;
+            } else {
+                regular++;
+            }
+        }
+
+        return processed + " processed | " +
+               nullSkipped + " null skipped | " +
+               faculty + " faculty | " +
+               regular + " regular";
+    }
+
+
+    public static void main(String[] args) {
+
+        LibraryMember m1 =
+            new LibraryMember(3);
+
+        System.out.println(m1.memberNumber);
+        System.out.println(
+            LibraryMember.getMembersEnrolled()
+        );
+
+        System.out.println(
+            LibraryMember.isValidRenewalCode("R12A")
+        );
+
+        System.out.println(
+            LibraryMember.isValidRenewalCode("R1A")
+        );
+
+        System.out.println(
+            LibraryMember.isValidRenewalCode("X12A")
+        );
+
+        m1.borrowBook();
+        m1.borrowBook("Fiction");
+
+        System.out.println(m1.getBooksBorrowed());
+
+        LibraryMember[] members = {
+            new FacultyMember(5, "Physics"),
+            null,
+            new LibraryMember(3)
+        };
+
+        System.out.println(
+            processNightlyAudit(members)
+        );
+    }
+}
